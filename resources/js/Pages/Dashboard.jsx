76644a9385c0142247a0a97cbd407534b4a51c83
ajikamaludin <aji19kamaludin@gmail.com>
2022-10-29 23:13:45 +0700

    fix dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -13,25 +13,25 @@ export default function Dashboard(props) {
             <div className='max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-between space-x-1'>
                 <div class="stats bg-base-100 border-base-300 border md:w-1/4">
                     <div class="stat">
-                        <div class="stat-title">Dokumen Aktif</div> 
+                        <div class="stat-title">Total Kunjugan Bulan Ini</div> 
                         <div class="stat-value">1,400</div> 
                     </div>
                 </div>
                 <div class="stats bg-base-100 border-base-300 border md:w-1/4">
                     <div class="stat">
-                        <div class="stat-title">Dokumen Diperbarui</div> 
+                        <div class="stat-title">Total Kunjugan Hari ini</div> 
                         <div class="stat-value">2,400</div> 
                     </div>
                 </div>
                 <div class="stats bg-base-100 border-base-300 border md:w-1/4">
                     <div class="stat">
-                        <div class="stat-title">Dokumen Berakhir</div> 
+                        <div class="stat-title">Total Order</div> 
                         <div class="stat-value">1,400</div> 
                     </div>
                 </div>
                 <div class="stats bg-base-100 border-base-300 border md:w-1/4">
                     <div class="stat">
-                        <div class="stat-title">Total Dokumen</div> 
+                        <div class="stat-title">Total Order Terbayar</div> 
                         <div class="stat-value">4,400</div> 
                     </div>
                 </div>
