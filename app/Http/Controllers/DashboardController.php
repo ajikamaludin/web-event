4517843c4fa167ec86e12575ee5f02688cd6850f
ajikamaludin <aji19kamaludin@gmail.com>
2022-11-06 14:07:29 +0700

    tracker

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Insight;
 use App\Models\Order;
 use Illuminate\Http\Request;
 
@@ -10,8 +11,8 @@ class DashboardController extends Controller
     public function index()
     {
         return inertia('Dashboard', [
-            'visit_today' => 0,
-            'visit_month' => 0,
+            'visit_today' => Insight::whereDate('created_at', now())->count(),
+            'visit_month' => Insight::whereMonth('created_at', now())->count(),
             'order_total' => Order::count(),
             'order_paid' => Order::where('order_status', Order::STATUS_PAID)->count()
         ]);
